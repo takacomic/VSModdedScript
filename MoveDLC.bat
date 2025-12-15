a587f16465fc2032e9e5648d50d729c83ff11bbb
takacomic <32620009+takacomic@users.noreply.github.com>
2025-12-14 23:32:38 -0600

    Add files via upload

--- MoveDLC.bat
+++ MoveDLC.bat
@@ -42,41 +42,41 @@ SET "FOLDERS=2230760 2313550 2690330 2887680 3210350 3451100"
 REM --- Move folders from Vampire Survivors to backup ---
 echo Backing up original folders from Vampire Survivors...
 FOR %%F IN (%FOLDERS%) DO (
-    IF EXIST "%VSORIGINAL%\%%F" (
-        robocopy "%VSORIGINAL%\%%F" "%VSBACKUP%\%%F" /MOVE /E
-        rmdir /S /Q "%VSORIGINAL%\%%F"
+    IF EXIST "%VSORIGINAL%%PATHSEP%%%F" (
+        robocopy "%VSORIGINAL%%PATHSEP%%%F" "%VSBACKUP%%PATHSEP%%%F" /MOVE /E
+        rmdir /S /Q "%VSORIGINAL%%PATHSEP%%%F"
     )
 )
 
 REM --- Move folders from VSModded to Vampire Survivors ---
 echo Swapping in modded folders...
 FOR %%F IN (%FOLDERS%) DO (
-    IF EXIST "%VSMODDED%\%%F" (
-        robocopy "%VSMODDED%\%%F" "%VSORIGINAL%\%%F" /MOVE /E
-        rmdir /S /Q "%VSMODDED%\%%F"
+    IF EXIST "%VSMODDED%%PATHSEP%%%F" (
+        robocopy "%VSMODDED%%PATHSEP%%%F" "%VSORIGINAL%%PATHSEP%%%F" /MOVE /E
+        rmdir /S /Q "%VSMODDED%%PATHSEP%%%F"
     )
 )
 
 REM --- Launch Vampire Survivors from VSModded and wait ---
 echo Launching Vampire Survivors...
-START /WAIT "" "%VSMODDED%\VampireSurvivors.exe"
+START /WAIT "" "%VSMODDED%%PATHSEP%VampireSurvivors.exe"
 
 REM --- Reverse the folder moves ---
 echo Reverting folders to original state...
 
 REM Move folders from Vampire Survivors back to VSModded
 FOR %%F IN (%FOLDERS%) DO (
-    IF EXIST "%VSORIGINAL%\%%F" (
-        robocopy "%VSORIGINAL%\%%F" "%VSMODDED%\%%F" /MOVE /E
-        rmdir /S /Q "%VSORIGINAL%\%%F"
+    IF EXIST "%VSORIGINAL%%PATHSEP%%%F" (
+        robocopy "%VSORIGINAL%%PATHSEP%%%F" "%VSMODDED%%PATHSEP%%%F" /MOVE /E
+        rmdir /S /Q "%VSORIGINAL%%PATHSEP%%%F"
     )
 )
 
 REM Move folders from backup back to Vampire Survivors
 FOR %%F IN (%FOLDERS%) DO (
-    IF EXIST "%VSBACKUP%\%%F" (
-        robocopy "%VSBACKUP%\%%F" "%VSORIGINAL%\%%F" /MOVE /E
-        rmdir /S /Q "%VSBACKUP%\%%F"
+    IF EXIST "%VSBACKUP%%PATHSEP%%%F" (
+        robocopy "%VSBACKUP%%PATHSEP%%%F" "%VSORIGINAL%%PATHSEP%%%F" /MOVE /E
+        rmdir /S /Q "%VSBACKUP%%PATHSEP%%%F"
     )
 )
 
